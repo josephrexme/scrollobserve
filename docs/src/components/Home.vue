192cf8fd70302f238636a2228a013565e6a1f31b
Joseph Rex <joerexko@gmail.com>
2017-10-18 07:18:15 -0500

    Add Vue documentation components

---
+++ docs/src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <scroll-indicator></scroll-indicator>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <div style="height:2000px">
+    </div>
+  </div>
+</template>
+
+<script>
+import ScrollIndicator from '@/components/ScrollIndicator.vue'
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'Test data'
+    }
+  },
+  components: { ScrollIndicator }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ docs/src/components/ScrollIndicator.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="scroll">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'scroll-indicator',
+  mounted() {
+    window.addEventListener('scroll', () => {
+      const pos = window.scrollY
+      const wh = window.innerHeight
+      const dh = document.body.scrollHeight
+      const perc = pos / (dh - wh)
+      this.$el.style.setProperty('--scale', perc + 0.1)
+    })
+  }
+}
+</script>
+
+<style scoped>
+.scroll{
+  --scale: 0.1;
+  position: fixed;
+  top: 0;
+  right: 100px;
+  width: 2px;
+  height: 100%;
+  background: white;
+  transform: scaleY(var(--scale));
+  transform-origin: top;
+}
+</style>
